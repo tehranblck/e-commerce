87833c1edcd4de23fa0389f1caf9fda9dd5066c7
Tehran Bayramov <nicosspyros@gmail.com>
2025-02-25 01:44:06 +0400

    sdff

--- app/(routes)/orders/page.tsx
+++ app/(routes)/orders/page.tsx
@@ -72,7 +72,7 @@ const OrdersPage: React.FC = () => {
     };
 
     return (
-        <div className="min-h-screen dark:bg-[#151515] text-gray-800 dark:text-white bg-white py-6 ">
+        <div className="min-h-screen bg-[#151515]  text-white py-6 ">
             <div className="max-w-4xl mx-auto px-4 py-8">
                 <h1 className="text-2xl font-semibold mb-6">Sifarişlərim</h1>
 
